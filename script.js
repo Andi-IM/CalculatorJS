d7afa0a7445980c161952836a3542f417cc96f69
Andi Irham <Andi.irhamm@gmail.com>
2020-05-11 09:49:25 +0700

    [Andi-IM] update script.js

--- script.js
+++ script.js
@@ -28,7 +28,67 @@ const inputNumber = (number) => {
 const operators = document.querySelectorAll(".operator");
 operators.forEach((operator) => {
     operator.addEventListener("click", (event) =>{
-        console.log(event.target.value);
+        inputOperator(event.target.value);
     });
 });
 
+const inputOperator = (operator) => {
+    if (calculationOperator === '') {
+        prevNumber = currentNumber;
+    }
+    calculationOperator = operator;
+    currentNumber = '0';
+}
+
+const calculate = () => {
+    let result = '';
+    switch (calculationOperator) {
+        case "+":
+            result = parseFloat(prevNumber) + parseFloat(currentNumber);
+            break;
+        case "-":
+            result = parseFloat(prevNumber) - parseFloat(currentNumber);
+            break;
+        case "*":
+            result = parseFloat(prevNumber) * parseFloat(currentNumber);
+            break;
+        case "/":
+            result = parseFloat(prevNumber) / parseFloat(currentNumber);
+            break;
+        default:
+            break;
+    }
+    currentNumber = result;
+    calculationOperator = '';
+}
+
+const equalSign = document.querySelector(".equal-sign");
+equalSign.addEventListener("click", ()=>{
+    calculate();
+    updateScreen(currentNumber);
+});
+
+const clearBtn = document.querySelector(".all-clear");
+clearBtn.addEventListener("click", ()=> {
+    clearAll();
+    updateScreen(currentNumber);
+});
+
+const clearAll = () =>{
+    prevNumber = '';
+    calculationOperator = '';
+    currentNumber = '0';
+}
+
+const decimal = document.querySelector(".decimal");
+decimal.addEventListener("click", (event) => {
+    inputDecimal(event.target.value);
+    updateScreen(currentNumber);
+});
+
+const inputDecimal = (dot) =>{
+    if (currentNumber.includes('.')) {
+        return
+    }
+    currentNumber += dot;
+}
